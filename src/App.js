809e2dcac80bf7b163890d8d57373911a47e3749
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-06-15 13:23:16 -0400

    digits

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i++) {
+      
+    }
+  }
   return (
     <div className="App">
       <div className="calculator">
         <div className="display">
           <span>(0)</span> 0
         </div>
+
         <div className="operators">
           <button>/</button>
           <button>*</button>
           <button>+</button>
           <button>-</button>
+
+          <button>DEL</button>
+        </div>
+
+        <div className="digits">
+          <button></button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </div>
     </div>
