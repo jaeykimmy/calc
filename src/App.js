bfa385a38abe2a984be7142cf65cc1eef05504e2
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-06-15 13:46:24 -0400

    css add

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     const digits = [];
 
     for (let i = 1; i < 10; i++) {
-      
+      digits.push(
+        <button key={i}>{i}</button>
+      )
     }
+    return digits
   }
   return (
     <div className="App">
@@ -26,7 +29,7 @@ function App() {
         </div>
 
         <div className="digits">
-          <button></button>
+          {createDigits()}
           <button>0</button>
           <button>.</button>
           <button>=</button>
